0ada2102c4485045c30cae5045f02c6ab37c9d94
hirbird <thxbrospam@gmail.com>
2024-03-22 02:32:11 -0400

    Updates

---
+++ _app/immutable/entry/start.YzkI9xWh.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DghjnlQg.js";export{t as start};
--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import { Page } from '../mod/frag/src/index.js'
-
-export class Page404 extends Page {
-    static href = '/404'
-    //static hidden = true
-    path = '/pages/404.html'
-}
